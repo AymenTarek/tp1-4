5c55e52f2da75661ab04bf545aa50d54e837d7a2
Aymen Beddar <bdrtarik@gmail.com>
2026-09-16 11:16:33 +0200

    Ajout attributs et constructeurs Fraction

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,8 +1,22 @@
 public class Fraction {
+    private int numérateur;
+    private int denominateur;
+    
+    public Fraction(int numérateur, int denominateur){
+        this.numérateur=numérateur;
+        this.denominateur=numérateur;
 
+    }
+    public Fraction(int numérateur){
+        this(numérateur,1);
+    }
+    public Fraction() {
+    this(0, 1);
+    }
     @Override
     public String toString() {
         return ("Je suis une fraction");
     }
+
 
 }
